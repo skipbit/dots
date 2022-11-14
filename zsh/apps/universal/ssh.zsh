1d1505bb8100b611ba2be6a8738d522d1f802c24
Yuma Endo <endo@skipbit.jp>
2022-11-14 16:44:04 +0900

    [tmux+ssh] remove old agent socks befer create symlink

--- zsh/apps/universal/ssh.zsh
+++ zsh/apps/universal/ssh.zsh
@@ -2,6 +2,7 @@
 # ssh-agent on tmux
 LOCAL_SSH_AUTH_SOCK="${HOME}/.ssh/ssh-agent-sock"
 if [ -S "${SSH_AUTH_SOCK}" ]; then
+    [ -f "${LOCAL_SSH_AUTH_SOCK}" ] && rm "${LOCAL_SSH_AUTH_SOCK}"
     case "${SSH_AUTH_SOCK}" in
         /tmp/*/agent.[0-9]*)
             ln -snf "${SSH_AUTH_SOCK}" "${LOCAL_SSH_AUTH_SOCK}" && export SSH_AUTH_SOCK="${LOCAL_SSH_AUTH_SOCK}"
