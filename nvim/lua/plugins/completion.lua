c9b742794060d90df88bcec5e31051e94833e229
Yuma Endo <endo@ai-ms.com>
2023-08-08 18:59:40 +0900

    [nvim] Fix conflict key binding

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -28,8 +28,8 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ['<C-p>'] = cmp.mapping.select_prev_item(),
         ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
