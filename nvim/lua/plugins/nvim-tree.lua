f35a1436fd88370a94a042a6af17d1aa9d4fe4cf
Yuma Endo <endo@ai-ms.com>
2023-08-29 18:47:15 +0900

    [nvim] Update nvim-tree config: enable window picker

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -13,7 +13,7 @@ require('nvim-tree').setup({
     actions = {
         open_file = {
             window_picker = {
-                enable = false,
+                enable = true,
             },
         },
     },
