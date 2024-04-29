978bc50e6efcb00f4720b76aa1925d6235e9f3c4
Yuma Endo <endo@skipbit.jp>
2024-04-29 15:25:36 +0900

    [nvim] Refactoring nvim-tree config

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -15,7 +15,7 @@ return {
                 },
             },
             view = {
-                side = left,
+                side = 'left',
                 width = 40,
             },
             renderer = {
@@ -24,15 +24,17 @@ return {
                     show = { file = false, folder = false, folder_arrow = true, git = false, modified = false }
                 },
             },
+            on_attach = function (bufnr)
+                local function opts(desc)
+                    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+                end
+
+                local api = require('nvim-tree.api')
+                api.config.mappings.default_on_attach(bufnr)
+
+                vim.keymap.set('n', '<C-s>', api.node.open.horizontal, opts('Open: Horizontal Split')) -- override: horizontal split (default: <C-X>)
+            end
         })
-
-        local api = require('nvim-tree.api')
-        local function opts(desc)
-            return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-        end
-
-        -- override: horizontal split (default: <C-X>)
-        vim.keymap.set('n', '<C-s>', api.node.open.horizontal, opts('Open: Horizontal Split'))
 
         vim.keymap.set('n', '<C-l>', ':NvimTreeToggle<CR>')
         vim.keymap.set('n', '<C-n>', ':NvimTreeFindFile<CR>')
