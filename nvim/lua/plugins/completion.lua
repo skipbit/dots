a2754f86058e94edef5da2a76e5e8c36ca9c5123
Yuma Endo <endo@skipbit.jp>
2024-04-29 17:38:34 +0900

    [nvim] Update completion vsnip to luasnip

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -5,14 +5,20 @@ return {
         'zbirenbaum/copilot-cmp',
     },
     {
+        'L3MON4D3/LuaSnip', dependencies = {
+            'saadparwaiz1/cmp_luasnip',
+            'rafamadriz/friendly-snippets',
+        }
+    },
+    {
         'hrsh7th/nvim-cmp', dependencies = {
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-cmdline',
-            'hrsh7th/vim-vsnip',
         },
         config = function ()
             local cmp = require('cmp')
+            require('luasnip.loaders.from_vscode').lazy_load()
 
             local has_words_before = function()
                 unpack = unpack or table.unpack
@@ -20,22 +26,18 @@ return {
                 return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
             end
 
-            local feed_key = function(key, mode)
-                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-            end
-
             cmp.setup({
                 snippet = {
                     expand = function(args)
-                        vim.fn['vsnip#anonymous'](args.body)
+                        require('luasnip').lsp_expand(args.body)
                     end
                 },
                 window = {
                     completion = cmp.config.window.bordered({
-                        border = 'single',
+                        border = 'rounded',
                     }),
                     documentation = cmp.config.window.bordered({
-                        border = 'single',
+                        border = 'rounded',
                     }),
                 },
                 mapping = cmp.mapping.preset.insert({
@@ -46,20 +48,29 @@ return {
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
                     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                    ['<Tab>'] = cmp.mapping(function(_)
+                    ['<Tab>'] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_next_item()
-                        elseif vim.fn['vsnip#available'](1) == 1 then
-                            feed_key('<Plug>(vsnip-expand-or-jump)', '')
                         elseif has_words_before() then
                             cmp.complete()
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                    ['<S-Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif has_words_before() then
+                            cmp.complete()
+                        else
+                            fallback()
                         end
                     end, { 'i', 's' }),
                 }),
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
                     { name = 'copilot' },
-                    { name = 'vsnip' },
+                    { name = 'luasnip' },
                 }, {
                     { name = 'buffer' },
                 }),
