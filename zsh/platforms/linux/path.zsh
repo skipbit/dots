37c51b8098dd7f39637557388bf1370afd192ee4
Yuma Endo <endo@ai-ms.com>
2024-10-18 19:42:28 +0900

    [brew] Add linux default path

--- zsh/platforms/linux/path.zsh
+++ zsh/platforms/linux/path.zsh
@@ -5,3 +5,9 @@ if [ -e "${HOME}/.fzf" ]; then
     path=("${HOME}/.fzf/bin" $path)
     fpath=("${HOME}/.fzf/shell" $fpath)
 fi
+
+# Homebrew
+path=("${HOME}/.homebrew/bin"(N-/) "${HOME}/.homebrew/sbin"(N-/) $path)
+if type brew > /dev/null 2>&1; then
+    fpath=("$(brew --prefix)/share/zsh/site-functions"(N-/) $fpath)
+fi
